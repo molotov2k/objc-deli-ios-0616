6c5ad336d98d9ec8ba46b981fc9a2afa2d107214
molotov2k <molotov2k@gmail.com>
2016-04-09 12:18:53 -0400

    Done.

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,36 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    if ([deliLine count] == 0) {
+        return @"The line is currently empty.";
+    }
+    
+    NSMutableString *line = [[NSMutableString alloc] initWithString:@"The line is:"];
+    for (NSUInteger i = 0; i < [deliLine count]; i++) {
+        [line appendFormat:@"\n%u. %@", i + 1, deliLine[i]];
+    }
+    
+    return line;
+}
+
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    
+    [deliLine addObject:name];
+}
+
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    
+    NSString *customer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    
+    return customer;
+}
+
+
 
 @end
